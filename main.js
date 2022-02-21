549dbd1466e3667764bf4f012f06dc578641598a
Jonas Petri <haren.jonas@gmail.com>
2022-02-21 15:50:56 +0100

    Small fix

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import './style.css'
-
 const bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
 "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const smallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
